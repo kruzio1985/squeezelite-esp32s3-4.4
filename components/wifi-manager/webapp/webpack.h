cce35ffc4a0478d740d8a48ac250cd4e243ec3c7
kruzio1985 <85479429+kruzio1985@users.noreply.github.com>
2026-01-15 00:07:07 +0100

    Add files via upload

---
+++ components/wifi-manager/webapp/webpack.h
@@ -0,0 +1,9 @@
+/***********************************
+webpack_headers
+dist/css/index.6d425ac534311a0131b2.css.gz,dist/favicon-32x32.png,dist/index.html.gz,dist/js/index.95ad03.bundle.js.gz,dist/js/node_vendors.95ad03.bundle.js.gz
+***********************************/
+#pragma once
+#include <inttypes.h>
+extern const char * resource_lookups[];
+extern const uint8_t * resource_map_start[];
+extern const uint8_t * resource_map_end[];
